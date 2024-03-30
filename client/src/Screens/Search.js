cfb05572448b565c139d12bf66eba854d22cde25
Antoine Bois--Berlioz <antoine.boisberlioz@gmail.com>
2024-03-30 16:54:57 +0100

    search bar upgrade

--- client/src/Screens/Search.js
+++ client/src/Screens/Search.js
@@ -1,25 +1,22 @@
 import React, { useState } from 'react';
+import '../App.css'; // Import CSS file
 
 function Search() {
   const [searchValue, setSearchValue] = useState('');
 
-  const handleSearch = (e) => {
+  // Function to handle changes in the input field
+  const handleChange = (e) => {
     setSearchValue(e.target.value);
   };
 
   return (
-    <div style={{ position: 'absolute', top: '10vw', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1000 }}>
+    <div className="searchContainer">
       <input
         type="text"
         placeholder="Search..."
         value={searchValue}
-        onChange={handleSearch}
-        style={{
-          width: '80vw', // Adjust width as needed
-          height: '7vw', // Adjust height as needed
-          borderRadius: '20px', // Adjust border radius as needed
-          padding: '10px', // Adjust padding as needed
-        }}
+        onChange={handleChange}
+        className="input"
       />
     </div>  
   );

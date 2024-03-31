4b9f9bf4c71408c6a41a57d0593d5809fe9fb6f7
Romi45 <106090803+Romi45@users.noreply.github.com>
2024-03-31 11:08:40 +0200

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,93 @@
+//this is the backend server, handles API endpoints and Mongoose connection
+// server.js
+
+// first we import our dependencies…
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const {Batiments, Salles} = require('./db_schema');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/hi', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+  //res.send("Hello world");
+});
+
+app.get('/money', (req,res) => {
+  res.send("Money");
+});
+
+app.get('/findplace', async(req,res) => {
+  try {
+
+    const {name} = req.query;
+
+    const regexPattern = new RegExp(name, 'i');
+
+    const query_batiments = { $or: [
+      { name: regexPattern },
+      { name2: regexPattern },
+      { dept: regexPattern },
+      // Add other fields you need to search here
+    ] };
+
+    const query_salles = { $or: [
+      { id: regexPattern},
+      { batiment : regexPattern},
+      { etage : regexPattern},
+      { usage : regexPattern},
+      { departement : regexPattern}
+    ]};
+
+    console.log(query_batiments, query_salles);
+    const data_batiments_raw = await Batiments.find(query_batiments);
+    const data_salles_raw = await Salles.find(query_salles);
+
+    console.log(data_batiments_raw);
+
+    const dataBatiments = data_batiments_raw.map(doc => ({
+      id: doc.id,
+      name: doc.name,
+      name2: doc.name2,
+      dept: doc.dept,
+      address: doc.address,
+      coordonnees: doc.coordonnees
+    }));
+
+    console.log("Data Batiments: \n", dataBatiments);
+    
+    const dataSalles = data_salles_raw.map(doc => ({
+      id: doc.id,
+      batiment: doc.batiment,
+      etage: doc.etage,
+      usage: doc.usage,
+      departement: doc.departement
+    }));
+    console.log("Data Salles\n", dataSalles);
+
+    const sentData = dataBatiments.concat(dataSalles);
+
+    //console.log(sentData);  
+    res.status(200).json(sentData);
+    //console.log(res);  
+  } catch (error) {
+    console.log("Error");
+    res.status(500).json({message: error.message})
+  }
+})
+
+
+
+mongoose.connect("mongodb+srv://jean:WEB_INSATLAS123@insatlas.ws8zgj1.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=INSAtlas")
+.then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(3001, () => {
+    console.log("Listening on port 3001");
+  });
+}).catch((error) => {
+  console.log(error);
+});
